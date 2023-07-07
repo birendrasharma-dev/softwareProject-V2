e3ac9408ead372a7515fec4edad6d9b9e944455b
Birendra <birendrasha444@gmail.com>
2023-07-07 13:18:55 +0545

    second commit

--- main.py
+++ main.py
@@ -2,34 +2,32 @@ from flask import Flask, render_template
 
 app = Flask("__name__")
 
-DOC =[
-  {
-    'id':1,
-    'name':'Birendra Sharma',
-    'info':'Heart Surgeon'
-  },
-  {
-    'id':2,
-    'name':'Bishal Regmi',
-    'info':' junior1 Heart Surgeon'
-  },
-  {
-    'id':3,
-    'name':'Anuj Thapa',
-    'info':'junior2 Heart Surgeon'
-  },
-  {
-    'id':4,
-    'name':'Biswas Kafle',
-    'info':'junior 3 Heart Surgeon'
-  }
-]
+DOC = [{
+  'id': 1,
+  'name': 'Birendra Sharma',
+  'info': 'Heart Surgeon',
+  'Email': 'birendrasha444@gmail.com'
+}, {
+  'id': 2,
+  'name': 'Bishal Regmi',
+  'info': ' Gynecologists',
+  'Email': 'bishalregmi180@gmail.com'
+}, {
+  'id': 3,
+  'name': 'Anuj Thapa',
+  'info': 'Ophthalmologists',
+  'Email': 'anujth345@gmail.com'
+}, {
+  'id': 4,
+  'name': 'Biswas Kafle',
+  'info': 'Otolaryngologists',
+  'Email': 'birendrasha444@gmail.com'
+}]
 
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html',
-                        jobs=DOC)
+  return render_template('home.html', jobs=DOC)
 
 
 if __name__ == "__main__":
